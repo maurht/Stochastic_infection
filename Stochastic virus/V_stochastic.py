81fe08c4b423da70c109aafb2d56e295e17c88c5
Mauricio Huicochea Toledo <84607002+maurht@users.noreply.github.com>
2021-10-11 17:47:53 -0500

    Update V_stochastic.py

--- Stochastic virus/V_stochastic.py
+++ Stochastic virus/V_stochastic.py
@@ -1,44 +1,25 @@
-import matplotlib.pyplot as plt
-import plotly.express as px
-
-from pandas import DataFrame
-from seaborn import scatterplot
-from tqdm import tqdm
 from population_class import Population
 
-
-sns.set_theme(style='white')
-
 model = Population(N=500,
-                   I0=10,
-                   box=1.5,
-                   tmax_infec=100)
+                   I0=3,
+                   box=1,
+                   infection_time=100,
+                   step_size=0.012,
+                   r_infec=0.07,
+                   p_infec=0.3,
+                   incubation_time=25,
+                   market=False,
+                   p_market=0.002,
+                   tmax_market=2,
+                   rnd_starting_infection_time=False)
 
-tmax = 1000
-for step in tqdm(range(tmax)):
-    model.be()
-    if model.In[-1] == 0:
-        print('Early stopping. The number of infected dropped to 0.')
-        break
 
-P = pd.DataFrame()
-P['X'] = [person.x for person in model.pop]
-P['Y'] = [person.y for person in model.pop]
-P['State'] = [person.est for person in model.pop]
+model.scatter_animation(interval=30)
 
-plt.figure(figsize=(12, 8))
-sns.scatterplot(data=P, x='X', y='Y', hue='State', palette='magma')
-plt.legend(bbox_to_anchor=(1.05, 0.5))
-plt.xlim([-model.box, model.box])
-plt.ylim([-model.box, model.box])
-plt.show()
+#model.run(run_time=200)
 
-lines = pd.DataFrame()
-lines['Suceptible'] = model.Su
-lines['Infected'] = model.In
-lines['Removed'] = model.Re
+#model.final_state()
 
-fig = px.line(lines)
-fig.show()
+#model.infection_timeline()
 
-print('Porcentaje de infeccion: ' + str((P.groupby('State').count()['X']['R'] / model.N) * 100))
+#model.infection_status()
