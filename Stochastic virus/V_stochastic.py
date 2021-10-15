9e837f523c4b489ebf02bcadcce213db2ed2a3c2
Mauricio Huicochea Toledo <84607002+maurht@users.noreply.github.com>
2021-10-15 12:01:23 -0500

    Update V_stochastic.py

--- Stochastic virus/V_stochastic.py
+++ Stochastic virus/V_stochastic.py
@@ -1,25 +1,25 @@
 from population_class import Population
 
 model = Population(N=500,
-                   I0=3,
+                   I0=1,
                    box=1,
                    infection_time=100,
-                   step_size=0.012,
-                   r_infec=0.07,
+                   step_size=0.01,
+                   r_infec=0.04,
                    p_infec=0.3,
-                   incubation_time=25,
-                   market=False,
+                   incubation_time=15,
+                   walk_type='bounce_walk',
                    p_market=0.002,
                    tmax_market=2,
                    rnd_starting_infection_time=False)
 
 
-model.scatter_animation(interval=30)
+model.scatter_animation(interval=20)
 
-#model.run(run_time=200)
+#model.run(run_time=1000)
 
 #model.final_state()
 
-#model.infection_timeline()
+model.infection_timeline()
 
-#model.infection_status()
+model.infection_status()
