b087df11d9159d5785991b1877ad77b7f4a3c7e4
Mauricio Huicochea Toledo <84607002+maurht@users.noreply.github.com>
2021-10-24 22:28:08 -0500

    Update population_class.py

--- Stochastic virus/population_class.py
+++ Stochastic virus/population_class.py
@@ -2,26 +2,26 @@ import numpy as np
 import random as rnd
 from indv_class import Indv
 from tqdm import tqdm
-from math import sqrt
 from matplotlib.animation import FuncAnimation
 import matplotlib.pyplot as plt
 from pandas import DataFrame
-import plotly.express as px
 
 
 def color_f(state):
     if state == 'S':
         return 0
     elif state == 'I':
-        return 10
+        return 1
     else:
-        return 5
+        return 0.5
 
 
 class Population:
 
-    def __init__(self, N, I0, box, step_size=0.05, r_infec=0.05, p_infec=0.1, infection_time=100, p_market=0.002
-                 , tmax_market=4, incubation_time=25, walk_type='rand_walk', rnd_starting_infection_time=False):
+    def __init__(self, N: int, I0: int, box: float, market_size: float, step_size=0.05, r_infec=0.05, p_infec=0.1,
+                 infection_time=100,
+                 p_market=0.002, tmax_market=4, incubation_time=25, walk_type='rand_bounce_walk',
+                 rnd_starting_infection_time=False):
 
         # Simulation parameters
 
@@ -29,38 +29,40 @@ class Population:
         self.I0 = I0  # Infectados iniciales
 
         # Variables for Indv class
-        self.box = box
+        self.box = box  # Tamaño del dominio
         self.step_size = step_size  # Tamaño del paso
         self.r_infec = r_infec  # Radio de infeccion
         self.p_infec = p_infec  # Probabilidad de infeccion
         self.p_market = p_market  # Probabilidad de ir al mercado
+        self.market_size = market_size
 
-        self.incubation_time = incubation_time
+        self.incubation_time = incubation_time  # tiempo de incubacion
         self.infection_time = infection_time  # Tiempo de duracion de la infeccion
-        self.tmax_market = tmax_market
+        self.tmax_market = tmax_market  # tiempo en el mercado
 
-        # Data record
+        # Status SIR
         self.Su = np.zeros(0, dtype=int)  # Desarrollo de poblacion suceptible a lo largo del tiempo
         self.In = np.zeros(0, dtype=int)  # Desarrollo de poblacion infectada a lo largo del tiempo
         self.Re = np.zeros(0, dtype=int)
 
-        # Special features
+        # Opciones especiales
         self.rnd_starting_infection_time = rnd_starting_infection_time
         self.walk_type = walk_type
 
         # setup
-        self.pop = self.populate()
+        self.pop = self.populate
 
+    @property
     def populate(self):
-
+        # Creacion de la poblacion
         pop = [Indv(step_size=self.step_size,
-                    r_infec=self.r_infec,
-                    p_infec=self.r_infec,
                     p_market=self.p_market,
                     box=self.box,
                     infection_time=self.infection_time,
                     tmax_market=self.tmax_market,
-                    incubation_time=self.incubation_time) for i in range(self.N)]  # Creacion de la poblacion
+                    incubation_time=self.incubation_time,
+                    walk_type=self.walk_type,
+                    market_size=self.market_size) for i in range(self.N)]
 
         # Asignacion de estatus I a I_O personas
         count = 0
@@ -79,36 +81,38 @@ class Population:
 
         return pop
 
-    # one instance of time
+    # una instancia de tiempo
 
     def be(self):
+        # Añdimos un espacio a los arrays del timeline SIR. No necesitamos calcular Re por ahora, ya que Si+Re+Su=N
         self.Su = np.append(self.Su, 0)
         self.In = np.append(self.In, 0)
 
+        # tipo de caminata elegido
         for person in self.pop:
-            if self.walk_type == 'market_walk':
-                person.market_walk()
-            elif self.walk_type == 'rand_walk':
-                person.rand_walk()
-            elif self.walk_type == 'bounce_walk':
-                person.bounce_walk()
-            else:
-                raise ValueError(
-                    'walk_type should be one of the supported classes : rand_walk, market_walk, bounce_walk')
+            person.walk()
+            ''' Proceso de infeccion. Por pasos: checamos si la persona es suceptible, luego tomamos a todos los
+            infectados que se mantienen al principio de self.pop por el .sort() que se usa cada vez que hay un
+            infectado, checamos si esa persona esta infectada (esto se hace en el caso de que el numero de infectados
+            baje a cero. Si esta condicion no se usa un individuo en estado Re puede infectar individuos), checamos
+            si el tiempo de incubacion se ha cumplido, si el caso entra en la probabilidad de infeccion y si la
+            persona esta dentro del radio de infeccion. Si las condiciones se cumplen, el estatus del individuo se
+            cambia, se ordena pop y se corta el ciclo'''
             if person.est == 'S':
-                for other_person in self.pop:
-                    if person.est != 'S':
-                        break
-                    elif other_person.est == 'I' and other_person.t > self.incubation_time and rnd.random() <= self.p_infec and sqrt(
-                            (person.x - other_person.x) ** 2 + (person.y - other_person.y) ** 2) <= self.r_infec:
+                for infected in self.pop[:self.In[-1] + 1]:
+                    if infected.est == 'I' and infected.t > self.incubation_time and rnd.random() <= self.p_infec and (
+                            (person.x - infected.x) ** 2 + (person.y - infected.y) ** 2) <= self.r_infec ** 2:
                         person.est = 'I'
+                        self.pop.sort(key=lambda x: x.est)
+                        break
+
             if person.est == 'S':
                 self.Su[-1] += 1
             elif person.est == 'I':
                 self.In[-1] += 1
         self.Re = np.append(self.Re, self.N - self.Su[-1] - self.In[-1])
 
-    def run(self, run_time):
+    def run(self, run_time: int):
         for _ in tqdm(range(run_time)):
             self.be()
             if self.In[-1] == 0:
@@ -116,29 +120,38 @@ class Population:
                 break
 
     def infection_status(self):
-        infection_count = sum([1 for person in self.pop if person.est == 'I'])
+        infection_count = sum([1 for person in self.pop if person.est == 'I' or person.est == 'R'])
         print('Porcentaje de infeccion: ' + str((infection_count / self.N) * 100))
 
-    def scatter_animation(self, interval):
+    def scatter_animation(self, interval: int, alpha: float, gradient=False):
         fig = plt.figure()
         ax = plt.axes(xlim=(-self.box, self.box), ylim=(-self.box, self.box))
-        # ax.legend(['S','I','R'], bbox_to_anchor=(1.1, 1.05))
-        scat = ax.scatter([], [])
+        if gradient:
+            scat = ax.scatter([self.box * 2, self.box * 3], [self.box * 2, self.box * 3], c=[0, self.infection_time],
+                              alpha=0.5)
+        else:
+            scat = ax.scatter([], [], alpha=alpha)
 
-        def update(i):
+        def update(_):
             scat.set_offsets(np.array([[person.x, person.y] for person in self.pop]))
-            scat.set_array(np.array([color_f(person.est) for person in self.pop]))
+            scat.set_array(np.array([person.t if gradient else color_f(person.est) for person in self.pop]))
             self.be()
 
         anim = FuncAnimation(fig, func=update, interval=interval)
+        plt.tight_layout()
         plt.show()
 
-    def final_state(self):
+    def final_state(self, gradient=False, palette='Spectral'):
         from seaborn import scatterplot
-        P = DataFrame()
-        P[['X', 'Y', 'State']] = [[person.x, person.y, person.est] for person in self.pop]
+        p = DataFrame()
+        if gradient:
+            p[['X', 'Y', 'Infection time']] = [[person.x, person.y, person.t] for person in self.pop]
+            scatterplot(data=p, x='X', y='Y', hue='Infection time', palette=palette)
+        else:
+            p[['X', 'Y', 'State']] = [[person.x, person.y, person.est] for person in self.pop]
+            scatterplot(data=p, x='X', y='Y', hue='State', palette='magma')
 
-        scatterplot(data=P, x='X', y='Y', hue='State', palette='magma')
+
         plt.legend(bbox_to_anchor=(1.05, 0.5))
         plt.xlim([-self.box, self.box])
         plt.ylim([-self.box, self.box])
@@ -146,6 +159,8 @@ class Population:
         plt.show()
 
     def infection_timeline(self):
+        import plotly.express as px
+
         lines = DataFrame()
         lines['Susceptible'] = self.Su
         lines['Infected'] = self.In
