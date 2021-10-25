6ca1c033b2df0ff2f62a5de28bb285473cbbb21a
Mauricio Huicochea Toledo <84607002+maurht@users.noreply.github.com>
2021-10-24 22:28:31 -0500

    Update indv_class.py

--- Stochastic virus/indv_class.py
+++ Stochastic virus/indv_class.py
@@ -1,15 +1,14 @@
 import random as rnd
 from math import sin, cos, pi
-
-def rnd_sign():
-    return 1 if rnd.random() < 0.5 else -1
+from numpy import sign
 
 
 # Definicion de un individuo
 
 class Indv:
 
-    def __init__(self, step_size, r_infec, p_infec, p_market, box, infection_time, tmax_market, incubation_time):
+    def __init__(self, step_size: float, p_market: float, box: float,
+                 infection_time: float, tmax_market: int, incubation_time: int, walk_type: str, market_size: float):
         self.box = box  # Tamaño de la region box x box
 
         # Coordenadas iniciales
@@ -18,45 +17,55 @@ class Indv:
         self.theta = 2 * pi * rnd.random()
 
         self.step_size = step_size  # Tamaño del paso
-        self.r_infec = r_infec  # Radio de infeccion
-        self.p_infec = p_infec  # Probabilidad de infeccion
         self.p_market = p_market  # Probabilidad de ir al mercado
         self.tmax_market = tmax_market  # Tiempo en el mercado(?)
+        self.market_size = market_size
 
-        self.infection_time = infection_time  # Tiempo de duracion de la infeccion
-        self.incubation_time = incubation_time
-        self.infection_incubation_time = self.incubation_time + self.infection_time
+        self.infection_incubation_time = incubation_time + infection_time
 
         self.est = 'S'  # Estado S-> suceptible, I-> infectado, R-> removido
-        self.xcp, ycp = 0, 0  # Coordenadas auxiliares
+        self.xcp, self.ycp = 0, 0  # Coordenadas auxiliares
+        self.box_save = box  # Valor de la caja auxiliar
         self.t = 0
         self.t_market = 0  # Tiempo en el mercado
 
-    def rand_walk(self):  # Caminata aleatoria
-        if self.est == 'I':
-            self.t += 1  # Contador de tiempo infectado
-        if self.t == self.infection_incubation_time:
-            self.est = 'R'  # El individuo se cura
-        random_theta = rnd.random() * pi *2
-        self.x += self.step_size * cos(random_theta)
-        self.y += self.step_size * sin(random_theta)
-        if abs(self.x) >= self.box:
-            self.x += -2 * self.step_size * cos(random_theta)
-        if abs(self.y) >= self.box:
-            self.y += -2 * self.step_size * sin(random_theta)
+        self.walk_type = walk_type
 
-    def market_walk(self):  # Caminata aleatoria pero existe la probabilidad de que
-        # el individuo visite un "mercado", que es un foco de infeccion
-        if rnd.random() <= self.p_market and self.t_market == 0:
-            self.xcp, self.ycp = self.x, self.y
-            self.x, self.y = 0, 0
-            self.t_market += 1
-        elif self.t_market < self.tmax_market and self.t_market != 0:
-            self.t_market += 1
-        elif self.t_market == self.tmax_market:
-            self.x, self.y = self.xcp, self.ycp
-            self.t_market = 0
-        else:
+    def walk(self):
+        if self.walk_type == 'rand_bounce_walk':  # Caminata aleatoria
+            if self.est == 'I':
+                self.t += 1  # Contador de tiempo infectado
+            if self.t == self.infection_incubation_time:
+                self.est = 'R'  # El individuo se cura
+            random_theta = rnd.random() * pi * 2
+            self.x += self.step_size * cos(random_theta)
+            self.y += self.step_size * sin(random_theta)
+            if abs(self.x) >= self.box:
+                self.x += -2 * self.step_size * cos(random_theta)
+            if abs(self.y) >= self.box:
+                self.y += -2 * self.step_size * sin(random_theta)
+
+        elif self.walk_type == 'rand_bounce_market_walk':
+            # Caminata aleatoria pero existe la probabilidad de que
+            # el individuo visite un "mercado", que es un foco de infeccion
+
+            if self.t_market == 0 and rnd.random() <= self.p_market:
+                self.xcp = self.x
+                self.ycp = self.y
+                self.box = self.market_size
+
+                self.x = self.market_size * (2 * rnd.random() - 1)
+                self.y = self.market_size * (2 * rnd.random() - 1)
+
+                self.t_market += 1
+            elif self.t_market < self.tmax_market and self.t_market != 0:
+                self.t_market += 1
+            elif self.t_market == self.tmax_market:
+                self.x = self.xcp
+                self.y = self.ycp
+                self.box = self.box_save
+                self.t_market = 0
+
             if self.est == 'I':
                 self.t += 1
             if self.t == self.infection_incubation_time:
@@ -67,20 +76,55 @@ class Indv:
             if abs(self.x) >= self.box:
                 self.x += -2 * self.step_size * cos(random_theta)
             if abs(self.y) >= self.box:
-                self.y += -2 * self.step_size * cos(random_theta)
+                self.y += -2 * self.step_size * sin(random_theta)
 
-    def bounce_walk(self):
-        if self.est == 'I':
-            self.t += 1  # Contador de tiempo infectado
-        if self.t == self.infection_incubation_time:
-            self.est = 'R'  # El individuo se cura
-        self.x += self.step_size * cos(self.theta)
-        self.y += self.step_size * sin(self.theta)
-        if abs(self.x) >= self.box:
-            self.theta = pi - self.theta
+        elif self.walk_type == 'linear_bounce_walk':
+            if self.est == 'I':
+                self.t += 1  # Contador de tiempo infectado
+            if self.t == self.infection_incubation_time:
+                self.est = 'R'  # El individuo se cura
             self.x += self.step_size * cos(self.theta)
             self.y += self.step_size * sin(self.theta)
-        if abs(self.y) >= self.box:
-            self.theta = - self.theta
+            if abs(self.x) >= self.box:
+                self.theta = pi - self.theta
+                self.x += self.step_size * cos(self.theta)
+                self.y += self.step_size * sin(self.theta)
+            if abs(self.y) >= self.box:
+                self.theta = - self.theta
+                self.x += self.step_size * cos(self.theta)
+                self.y += self.step_size * sin(self.theta)
+
+        elif self.walk_type == 'rand_toroid_walk':
+            if self.est == 'I':
+                self.t += 1  # Contador de tiempo infectado
+            if self.t == self.infection_incubation_time:
+                self.est = 'R'  # El individuo se cura
+            random_theta = rnd.random() * pi * 2
+            self.x += self.step_size * cos(random_theta)
+            self.y += self.step_size * sin(random_theta)
+            if abs(self.x) >= self.box:
+                self.x += - 2 * sign(self.x) * self.box
+            if abs(self.y) >= self.box:
+                self.y += - 2 * sign(self.y) * self.box
+
+        elif self.walk_type == 'linear_toroid_walk':
+            if self.est == 'I':
+                self.t += 1  # Contador de tiempo infectado
+            if self.t == self.infection_incubation_time:
+                self.est = 'R'  # El individuo se cura
             self.x += self.step_size * cos(self.theta)
             self.y += self.step_size * sin(self.theta)
+            if abs(self.x) >= self.box:
+                self.x += - 2 * sign(self.x) * self.box
+            if abs(self.y) >= self.box:
+                self.y += - 2 * sign(self.y) * self.box
+
+        elif self.walk_type == 'none':
+            if self.est == 'I':
+                self.t += 1  # Contador de tiempo infectado
+            if self.t == self.infection_incubation_time:
+                self.est = 'R'  # El individuo se cura
+
+        else:
+            raise ValueError('walk_type should be one of the supported classes : rand_bounce_walk, '
+                             'rand_bounce_market_walk, linear_bounce_walk, rand_toroid_walk, linear_toroid_walk, none')
